959220b01bd77ca902b3285fce4fdc1f256bdf66
Daniel Sobe <daniel.sobe@googlemail.com>
2023-01-14 11:41:08 +0100

    prěni pospyt za wutworjenje knihi

--- scripts/04_latex.sh
+++ scripts/04_latex.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-rm -f 04_out_pdf
+INFOLDER="../žórła/"
+MAINFILENAME="swjate_pismo"
+
+rm -rf 04_out_pdf
 mkdir -p 04_out_pdf
 
-cp test.tex 04_out_pdf
+cp $INFOLDER/* 04_out_pdf
 pushd 04_out_pdf
-PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux/ latex test.tex
-PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux/ dvipdf test.dvi
+PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux/ latex $MAINFILENAME.tex
+PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux/ dvipdf $MAINFILENAME.dvi
 popd
 
-rm -f 04_out_epub
+rm -rf 04_out_epub
 mkdir -p 04_out_epub
 
-cp test.tex 04_out_epub
+cp $INFOLDER/* 04_out_epub
 pushd 04_out_epub
-PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux/ tex4ebook test.tex
+PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux/ tex4ebook $MAINFILENAME.tex
 popd
 
-rm -f 04_out_html
+rm -rf 04_out_html
 mkdir -p 04_out_html
 
-cp test.tex 04_out_html
+cp $INFOLDER/* 04_out_html
 pushd 04_out_html
-PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux/ htlatex test.tex
+PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux/ htlatex $MAINFILENAME.tex
 popd
 
 
