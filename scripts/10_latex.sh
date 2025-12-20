11dcdb8268e0439e794044ac13e6af37300a5a02
Daniel Sobe <hardcoredaniel@seznam.cz>
2025-12-20 13:31:24 +0100

    include not corrected parts in build

--- scripts/10_latex.sh
+++ scripts/10_latex.sh
@@ -10,7 +10,8 @@ convert_to_latex() {
 	    060_sc_sw_marka.txt 061_sc_sw_lukasa.txt 062_sc_sw_jana.txt \
 	    063_japost_stucki.txt 064_l_paw_romskich.txt 065_p_l_paw_korinth.txt \
 	    066_d_l_paw_korinth.txt 067_l_paw_salatis.txt 068_l_paw_ephesis.txt \
-	    069_l_paw_philipp.txt 070_l_paw_kolosej.txt 071_p_l_paw_thesalon.txt ; do
+	    069_l_paw_philipp.txt 070_l_paw_kolosej.txt 071_p_l_paw_thesalon.txt \
+	    072_d_l_thesalon-NOTCORR.txt 073_p_l_paw_timothej-NOTCORR.txt 074_d_l_paw_timothej-NOTCORR.txt ; do
 		echo $i;
 #		(cat $i | sed -e s/\)\)\)\)/\}\}/g \
 #		              -e s/\)\)/\}/g \
