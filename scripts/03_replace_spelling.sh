ccdc501127889ba5bcf21b2cdbb98957c4fc7348
Daniel Sobe <hardcoredaniel@seznam.cz>
2026-05-24 16:23:15 +0200

    print out when script has processed mandatory parts

--- scripts/03_replace_spelling.sh
+++ scripts/03_replace_spelling.sh
@@ -6,4 +6,9 @@ mkdir -p 03_out
 for i in $(find 02_out/ -name "*.txt" | sort); do
 	echo $i;
 	perl ./03_replace_spelling.pl $i $(echo $i | sed -e s/02_out/03_out/)
+	if [ "$i" = "02_out/058_n_t_knihi.txt" ]; then
+		echo "=================================================="
+		echo "Script can be interrupted, all non-finished files processed!"
+		echo "=================================================="
+	fi
 done
